866fb003317900d65d83e2e05bdf2f846ff38faa
chaikaexe <49320557+chaikaexe@users.noreply.github.com>
2022-12-16 13:02:09 +0300

    Add files via upload

---
+++ dom2.1.pas
@@ -0,0 +1,16 @@
+﻿begin
+ var a: array of integer;
+ var z,x,raz:integer;
+ var n:=ReadInteger;
+ SetLength(a,n);
+ for var i:=0 to n-1 do a[i]:=ReadInteger;
+z:=0;
+x:=1;
+raz:=abs(a[z]-a[x]);
+  for var i:=1 to n-1 do
+     for var j:=i+1 to n do
+    if raz>abs(a[i-1]-a[j-1]) then begin
+      z:=i-1; x:=j-1; raz:=abs(a[i-1]-a[j-1]);
+    end;
+  print(a[z],a[x]);
+end.
---
+++ dom2.2(end).pas
@@ -0,0 +1,22 @@
+﻿var s:=ReadLnString('Введите строку: ');
+var ps:=ReadLnString('Введите подстроку: ');
+var t:='';
+var c:=1;
+var k:=0;
+var countPs:=0;
+begin
+  var i:=1;
+while i<=Length(s) do begin
+   k:=0;
+   while ((i+k)<=Length(s)) and (k<Length(ps)) and (s[i+k]=ps[k+1]) do
+        inc(k);
+   if k=Length(ps) then Inc(countPs);
+   if (k=Length(ps)) and not odd(countPs) then inc(i,k)
+   else begin
+     t:=t+s[i];
+     inc(i);
+     end;
+   end;
+s:=t;
+writeln(s);
+end.
